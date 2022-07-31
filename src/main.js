40cca0b757c4115294d2845e3c670ef097df954f
yeah1997 <lee7778889@gmail.com>
2022-07-31 15:14:40 +0900

    Init project- Import Vant Componnet

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import '@/styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
+
+Vue.use(Vant)
 
 new Vue({
   router,
